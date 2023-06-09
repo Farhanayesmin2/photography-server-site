51660236518d4ea00e9fb20c80c16c4b4246f6de
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-06-09 23:52:32 +0600

    Get instructors data from mongodb

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 4000;
 
-
+//This is very very important to connect database for secure pass and user
+require("dotenv").config();
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.gojv5gq.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,30 +18,45 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
+
+
+
+
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
+
+   const instructorsCollection = client.db("schoolPhotography").collection("instructorCollection");
+  
+ // Get all instructorsCollection data 
+    app.get("/instructors", async (req, res) => {
+      const query = {};
+      const cursor = instructorsCollection.find(query);
+      const instructor = await cursor.toArray();
+      res.send(instructor);
+    });
+
+
+     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 // Set running data or not
 app.get("/", (req, res) => {
   res.send("api running in port 5000");
 });
 
-// Thats the main port for run data
+// this is the main port for run data
 app.listen(port, () => {
   console.log("The port is runing on:", port);
 });
