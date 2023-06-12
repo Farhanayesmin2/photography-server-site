42959b660a063a00574ba669fe6132c63f94281d
Farhanayesmin2 <farhanayesmin@gmail.com>
2023-06-12 12:27:55 +0600

    Payment stripe implement

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-const port = process.env.PORT || 4000;
 const { ObjectId } = require("mongodb");
+const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.PAYMENT_CREATE_INTENT_KEY);
+const port = process.env.PORT || 4000;
+
+
+
+
 //This is very very important to connect database for secure pass and user
 require("dotenv").config();
 // middleware
@@ -22,7 +28,24 @@ const client = new MongoClient(uri, {
 });
 
 
+function verifyJWT(req, res, next) {
 
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('unauthorized access');
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.JWT_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 
 async function run() {
@@ -31,7 +54,7 @@ async function run() {
    const instructorsCollection = client.db("schoolPhotography").collection("instructorCollection");
    const usersData = client.db("schoolPhotography").collection("users");
    const classData = client.db("schoolPhotography").collection("classCollection");
-  
+   const paymentsCollection = client.db('schoolPhotography').collection('payments');
  // Get all instructorsCollection data 
     app.get("/instructors", async (req, res) => {
       const query = {};
@@ -61,7 +84,7 @@ async function run() {
     });
 
    // Get  my class data
-    app.get("/myclass", async (req, res) => {
+    app.get("/dashboard/myclass", async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const cursor = classData.find(query);
@@ -70,7 +93,7 @@ async function run() {
     });
 
      // my class data delete 
-    app.delete("/myclass/:id", async (req, res) => {
+    app.delete("/dashboard/myclass/:id", async (req, res) => {
       const id = req.params.id;
       console.log("please delete from database", id);
       const query = { _id: new ObjectId(id) };
@@ -78,16 +101,57 @@ async function run() {
       res.send(result);
     });
     
-     app.get('/myclass/:id', async (req,res)=>{
+     app.get('/dashboard/myclass/:id', async (req,res)=>{
             const id = req.params.id;
             const query = { _id: new  ObjectId(id)};
             const select = await classData.findOne(query);
             res.send(select)
         })
 
+  
     
-    
+      app.post('/create-payment-intent', async (req, res) => {
+  const select = req.body;
+  const price = select.price;
+  const amount = price * 100;
 
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      currency: 'usd',
+      amount: amount,
+      payment_method_types: ['card'],
+    });
+    
+    res.send({
+      clientSecret: paymentIntent.client_secret,
+    });
+  } catch (error) {
+    console.error('Error creating payment intent:', error);
+    res.status(500).send('Error creating payment intent');
+  }
+});
+
+     app.post('/payments', async (req, res) =>{
+            const payment = req.body;
+            const result = await paymentsCollection.insertOne(payment);
+            // const id = payment.selectId
+            // const collectionId = payment.collectionId
+            // const filter2 = {collectionId: collectionId}
+            // const filter3 = {_id: ObjectId(collectionId)}
+            // const filter4 = {_id: ObjectId(collectionId)}
+            // const filter = {_id: ObjectId(id)}
+            // const updatedDoc = {
+            //     $set: {
+            //         paid: true,
+            //         transactionId: payment.transactionId
+            //     }
+            // }
+            // const deleteResult = await phoneCollections.deleteOne(filter3)
+            // const updatedResult2 = await bookingCollections.updateMany(filter2, updatedDoc)
+            // const updatedResult = await bookingCollections.updateOne(filter, updatedDoc)
+            // const updatedResult3 = await myProductCollections.updateOne(filter4, updatedDoc)
+            res.send(result);
+        })
 
 
   
@@ -102,10 +166,28 @@ async function run() {
 
 
 
+// const crypto = require('crypto');
+
+// const generateRandomSecretKey = () => {
+//   const byteLength = 32;
+//   return crypto.randomBytes(byteLength).toString('hex');
+// };
+
+// const secretKey = generateRandomSecretKey();
+// console.log(secretKey);
 
 
 
-
+ app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersData.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.JWT_TOKEN, { expiresIn: '1h' })
+                return res.send({ accessToken: token });
+            }
+            res.status(403).send({ accessToken: '' })
+        });
 
 
 
